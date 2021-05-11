fed68afb511429726de7b07d0e6ada75c2d8f1d8
jpcnmm2 <83856390+jpcnmm2@users.noreply.github.com>
2021-05-11 14:16:42 +0800

    Update txstockCOOKIE.js

--- txstockCOOKIE.js
+++ txstockCOOKIE.js
@@ -1,27 +1,19 @@
 //独立COOKIE文件     ck在``里面填写，多账号换行
-let userheaderVal= `oA0GbjpHujezy-GAgJXJPkRUGlvs&_appName=ios&_dev=iPhone11,8&_devId=6640a3384872fecf13f652b28bd6e5783773130e&_appver=8.8.1&_ifChId=&_isChId=1&_osVer=14.3&openid=oA0GbjpHujezy-GAgJXJPkRUGlvs&fskey=v0aaf63c222602f59838fbc86d9192ff&appid=wxcbc3ab3807acb685&access_token=42_PVkMIZ5j2HtEfmDBDR1fGG4cg7kelpt4fgQg6Avi0kkGgzygR3vzZaMQNaaXSKnt9vSLpTn5aVmQZ0dkhv-HfmgTLAkfBW6UtcppN8D1-Pg&buildType=store&check=11&_idfa=00000000-0000-0000-0000-000000000000&lang=zh_CN
-oA0GbjtyzzRMaKkzCHhfTkMHjkTA&_appName=ios&_dev=iPhone6,1&_devId=9090ed05a0c144dcae5f34c82609470dd7c85f4b&_appver=8.8.1&_ifChId=&_isChId=1&_osVer=12.4.7&openid=oA0GbjtyzzRMaKkzCHhfTkMHjkTA&fskey=v0aaf63c22160332d6b9ce48c835a554&appid=wxcbc3ab3807acb685&access_token=42_9dZqO484ZIfSXfJdEBgI8U307DL0LJe-qhKWBskcUxgMOJNuZfB_qRHHsFKxpcwU1Zfg99U-ijo-7ei09RXw4NV3-DC-zNBmV49lvo4ELcE&buildType=store&check=11&_idfa=8DF7094B-4B16-4AD7-9BE6-157666EDA71C&lang=zh_CN`
+let userheaderVal= ``
 
-let userkeyVal= `pgv_pvid=5853813260
-pgv_pvid=6230997862`
+let userkeyVal= ``
 
-let cashheaderVal= `oA0GbjpHujezy-GAgJXJPkRUGlvs&fskey=v0aaf63c222602f59838fbc86d9192ff&access_token=42_PVkMIZ5j2HtEfmDBDR1fGG4cg7kelpt4fgQg6Avi0kkGgzygR3vzZaMQNaaXSKnt9vSLpTn5aVmQZ0dkhv-HfmgTLAkfBW6UtcppN8D1-Pg&_appName=ios&_appver=8.8.1&_osVer=14.3&_devId=6640a3384872fecf13f652b28bd6e5783773130e
-oA0GbjtyzzRMaKkzCHhfTkMHjkTA&fskey=v0aaf63c22160332d6b9ce48c835a554&access_token=42_9dZqO484ZIfSXfJdEBgI8U307DL0LJe-qhKWBskcUxgMOJNuZfB_qRHHsFKxpcwU1Zfg99U-ijo-7ei09RXw4NV3-DC-zNBmV49lvo4ELcE&_appName=ios&_appver=8.8.1&_osVer=12.4.7&_devId=9090ed05a0c144dcae5f34c82609470dd7c85f4b`
+let cashheaderVal= ``
 
-let signheaderVal= `oA0GbjpHujezy-GAgJXJPkRUGlvs&fskey=v0aaf63c222602f59838fbc86d9192ff&channel=1&access_token=42_PVkMIZ5j2HtEfmDBDR1fGG4cg7kelpt4fgQg6Avi0kkGgzygR3vzZaMQNaaXSKnt9vSLpTn5aVmQZ0dkhv-HfmgTLAkfBW6UtcppN8D1-Pg&_appName=ios&_appver=8.8.1&_osVer=14.3&_devId=6640a3384872fecf13f652b28bd6e5783773130e
-oA0GbjtyzzRMaKkzCHhfTkMHjkTA&fskey=v0aaf63c22160332d6b9ce48c835a554&channel=1&access_token=42_9dZqO484ZIfSXfJdEBgI8U307DL0LJe-qhKWBskcUxgMOJNuZfB_qRHHsFKxpcwU1Zfg99U-ijo-7ei09RXw4NV3-DC-zNBmV49lvo4ELcE&_appName=ios&_appver=8.8.1&_osVer=12.4.7&_devId=9090ed05a0c144dcae5f34c82609470dd7c85f4b`
+let signheaderVal= ``
 
-let signkeyVal= `pgv_pvid=5853813260; ts_last=/activity/page/activityForward/; ts_refer=zqact01.tenpay.com/activity/page/appNewcomerSpree/; ts_sid=2819936403; ts_uid=8237901996
-pgv_info=ssid=s8876794070; pgv_pvid=6230997862; ts_last=/activity/page/guessRiseFall/; ts_sid=3337344911; ts_uid=4368122720`
+let signkeyVal= ``
 
-let taskheaderVal= `&_dev=iPhone11,8&_devId=6640a3384872fecf13f652b28bd6e5783773130e&_appver=8.8.1&_ifChId=&_isChId=1&_osVer=14.3&openid=oA0GbjpHujezy-GAgJXJPkRUGlvs&fskey=v0aaf63c222602f59838fbc86d9192ff&appid=wxcbc3ab3807acb685&access_token=42_PVkMIZ5j2HtEfmDBDR1fGG4cg7kelpt4fgQg6Avi0kkGgzygR3vzZaMQNaaXSKnt9vSLpTn5aVmQZ0dkhv-HfmgTLAkfBW6UtcppN8D1-Pg&buildType=store&check=11&_idfa=00000000-0000-0000-0000-000000000000&lang=zh_CN
-&_dev=iPhone6,1&_devId=9090ed05a0c144dcae5f34c82609470dd7c85f4b&_appver=8.8.1&_ifChId=&_isChId=1&_osVer=12.4.7&openid=oA0GbjtyzzRMaKkzCHhfTkMHjkTA&fskey=v0aaf63c22160332d6b9ce48c835a554&appid=wxcbc3ab3807acb685&access_token=42_9dZqO484ZIfSXfJdEBgI8U307DL0LJe-qhKWBskcUxgMOJNuZfB_qRHHsFKxpcwU1Zfg99U-ijo-7ei09RXw4NV3-DC-zNBmV49lvo4ELcE&buildType=store&check=11&_idfa=8DF7094B-4B16-4AD7-9BE6-157666EDA71C&lang=zh_CN`
+let taskheaderVal= ``
 
-let taskkeyVal= `pgv_info=ssid=s9855562537; pgv_pvid=5853813260
-pgv_pvid=6230997862`
+let taskkeyVal= ``
 
-let wxtaskkeyVal= `wzq_channel=fm_wzq_wx_v1_unknow_01..; qlappid=wx9cf8c670ebd68ce4; qlskey=v6474a41e11602f5ba2745f1df1c9c01; qluin=085e9858ef6105427a4552ccb@wx.tenpay.com; qq_logtype=16; wx_session_time=1613716386000; wzq_qlappid=wx9cf8c670ebd68ce4; wzq_qlskey=v6474a41e11602f5ba2745f1df1c9c01; wzq_qluin=os-ppuHDln1oa1RzHrRwd6eNmwNA
-wzq_channel=fm_wzq_wx_v1_unknow_01..; qlappid=wx9cf8c670ebd68ce4; qlskey=v0aaf8a822260332f23c0dc45f10eb51; qluin=085e9858ec55f05a45940d773@wx.tenpay.com; qq_logtype=16; wx_session_time=1613967139000; wzq_qlappid=wx9cf8c670ebd68ce4; wzq_qlskey=v0aaf8a822260332f23c0dc45f10eb51; wzq_qluin=os-ppuBax9pwQDdda1m05OypAXS8`
+let wxtaskkeyVal= ``
 
 
 let cookieArr = {
