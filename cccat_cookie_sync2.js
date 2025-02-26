0f79ba66327faca0f12775a33f892dcf99aebcbd
jpcnmm2 <83856390+jpcnmm2@users.noreply.github.com>
2025-02-26 10:34:17 +0800

    Update cccat_cookie_sync2.js

--- cccat_cookie_sync2.js
+++ cccat_cookie_sync2.js
@@ -66,7 +66,7 @@ async function getScriptUrl() {
   //console.log(`=======================清空环境变量=======================`);
   const Ids = response.data[0].id;
   //console.log(Ids);
-  const CCCAT_cookie = [{"name":"CCCAT_COOKIE", "value":cookie, "id":Ids,"remarks":""}]
+  const CCCAT_cookie = {"name":"CCCAT_COOKIE", "value":cookie, "id":Ids,"remarks":""}
   console.log(CCCAT_cookie);
   const response2 = await $.ql.edit(CCCAT_cookie);
   console.log(response2);
