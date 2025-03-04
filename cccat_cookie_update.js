61b32747ce3af8ce3a48edf0fd4dd8a31f5d9d9c
jpcnmm2 <83856390+jpcnmm2@users.noreply.github.com>
2025-03-04 10:03:39 +0800

    Update cccat_cookie_update.js

--- cccat_cookie_update.js
+++ cccat_cookie_update.js
@@ -27,7 +27,7 @@ JD-Cookie = type=http-request, pattern=^https?:\/\/api\.m\.jd\.com\/client\.acti
 
 #Loon
 
-http-request ^https?:\/\/cccat\.io\/user\/index\.php script-path=https://raw.githubusercontent.com/jpcnmm2/jscripts/refs/heads/main/cccat_cookie_sync.js
+http-request ^https?:\/\/cccat\.io\/user\/index\.php script-path=https://raw.githubusercontent.com/jpcnmm2/jscripts/refs/heads/main/cccat_cookie_update.js
 
 [MITM]
 hostname = %APPEND% cccat.io
