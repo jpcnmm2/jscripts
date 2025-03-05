e0eb7a037ccdfc5c5d2c852efcbbee342b18f189
jpcnmm2 <83856390+jpcnmm2@users.noreply.github.com>
2025-03-06 00:12:52 +0800

    Update Shijuanbao.js

--- Shijuanbao.js
+++ Shijuanbao.js
@@ -5,7 +5,7 @@ ios151 密钥来源@三岁
 
 [rewrite_local]
 
-https://sjb-api.aizuoye100.com/api/v2/application/get_user url script-response-body https://raw.githubusercontent.com/jpcnmm/jscripts/main/Shijuanbao.js
+https://sjb-api.aizuoye100.com/api/v2/application/get_user url script-response-body https://raw.githubusercontent.com/jpcnmm2/jscripts/refs/heads/main/Shijuanbao.js
 
 
 [mitm]
