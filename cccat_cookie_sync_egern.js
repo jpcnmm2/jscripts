858f2d94a07ce59779261979f2ca37c9c2ba7b82
jpcnmm2 <83856390+jpcnmm2@users.noreply.github.com>
2026-03-30 14:17:26 +0800

    Update cccat_cookie_sync_egern.js

--- cccat_cookie_sync_egern.js
+++ cccat_cookie_sync_egern.js
@@ -64,7 +64,7 @@ export default async function (ctx) {
 	
 	
 	
-  const resp = await ctx.http.get(`${$.ql_url}/open/auth/token?client_id=${$.ql_config.client_id}&client_secret=${$.ql_config.client_secret}`, { headers:{"Content-Type": `application/json;charset=UTF-8`}});	        });
+  const resp = await ctx.http.get(`${$.ql_url}/open/auth/token?client_id=${$.ql_config.client_id}&client_secret=${$.ql_config.client_secret}`, { headers:{"Content-Type": `application/json;charset=UTF-8`}});
   const respdata = await resp.json();
   if (respdata.code ===200) {
 	  $.log(`登陆成功`);
