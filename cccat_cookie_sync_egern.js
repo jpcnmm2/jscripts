873bfaf69a8add41a11e52e6c3d833a649137f01
jpcnmm2 <83856390+jpcnmm2@users.noreply.github.com>
2026-03-30 20:52:10 +0800

    Update cccat_cookie_sync_egern.js

--- cccat_cookie_sync_egern.js
+++ cccat_cookie_sync_egern.js
@@ -91,7 +91,7 @@ export default async function (ctx) {
   console.log(resp);
   
   if (resp.code == 200) {  
-     rescron = await ctx.http.get(`${$.ql_url}/open/crons?searchValue=autocheckin`, { headers:{"Content-Type": `application/json;charset=UTF-8`, "Authorization": `Bearer ${ql_token}`}});
+     let rescron = await ctx.http.get(`${$.ql_url}/open/crons?searchValue=autocheckin`, { headers:{"Content-Type": `application/json;charset=UTF-8`, "Authorization": `Bearer ${ql_token}`}});
   rescron = await respcron.json();
   console.log("任务查询结果：");
   console.log(rescron);
@@ -100,8 +100,8 @@ export default async function (ctx) {
   console.log("任务列表：");
   console.log(taskIDs);
     
-  resp =  await ctx.http.put(`${$.ql_url}/open/envs`, {headers:{"Content-Type": `application/json;charset=UTF-8`, "Authorization": `Bearer ${ql_token}`}, body: `${taskIDs}`});
-  resrun = await resp.json();	  
+  resp =  await ctx.http.put(`${$.ql_url}/open/envs`, {headers:{"Content-Type": `application/json;charset=UTF-8`, "Authorization": `Bearer ${ql_token}`}, body: JSON.stringify(taskIDs)});
+  let resrun = await resp.json();	  
   console.log("执行响应：")
   console.log(resrun)
   return $.notify(title, '更新成功🎉', ``);
@@ -113,43 +113,6 @@ export default async function (ctx) {
 
 /* prettier-ignore */
 function ENV(){const isJSBox=typeof require=="function"&&typeof $jsbox!="undefined";return{isQX:typeof $task!=="undefined",isLoon:typeof $loon!=="undefined",isSurge:typeof $httpClient!=="undefined"&&typeof $utils!=="undefined",isBrowser:typeof document!=="undefined",isNode:typeof require=="function"&&!isJSBox,isJSBox,isRequest:typeof $request!=="undefined",isScriptable:typeof importModule!=="undefined",isShadowrocket:"undefined"!==typeof $rocket,isStash:"undefined"!==typeof $environment&&$environment["stash-version"]}}
-
-/* function ENV(){
-	const isJSBox=typeof require=="function"&&typeof $jsbox!="undefined";	
-	const getEnv = () =>
-    "undefined" != typeof $environment && $environment["surge-version"]
-      ? "Surge"
-      : "undefined" != typeof $environment && $environment["stash-version"]
-      ? "Stash"
-      : "undefined" != typeof document
-      ? "Browser"
-	  : eval('typeof require=="function"&&!isJSBox')
-      ? "Node.js"
-	  : "undefined" != typeof $request
-      ? "Request"
-	  : "undefined" != typeof importModule
-      ? "Scriptable"
-      : "undefined" != typeof $task
-      ? "Quantumult X"
-      : "undefined" != typeof $loon
-      ? "Loon"
-      : "undefined" != typeof $rocket
-      ? "Shadowrocket"
-//      : void 0,
-      : "Surge",
-	  
-	  isQX = () => "Quantumult X" === getEnv(),
-	  isLoon = () => "Loon" === getEnv(),
-	  isSurge = () => "Surge" === getEnv(),
-	  isBrowser = () => "Browser" === getEnv(),
-	  isNode = () => "Node.js" === getEnv(),
-	  isRequest = () => "Request" === getEnv(),
-	  isScriptable = () => "Scriptable" === getEnv(),
-	  isStash = () => "Stash" === getEnv(),
-	  isShadowrocket = () => "Shadowrocket" === getEnv();
-	
-	return{isQX,isLoon,isSurge,isBrowser,isNode,isJSBox,isRequest,isScriptable,isShadowrocket,isStash}		
-} */
 
 /* prettier-ignore */
 function HTTP(defaultOptions={baseURL:""}){const{isQX,isLoon,isSurge,isScriptable,isNode,isBrowser,isShadowrocket,isStash,}=ENV();const methods=["GET","POST","PUT","DELETE","HEAD","OPTIONS","PATCH"];const URL_REGEX=/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/;function send(method,options){options=typeof options==="string"?{url:options}:options;const baseURL=defaultOptions.baseURL;if(baseURL&&!URL_REGEX.test(options.url||"")){options.url=baseURL?baseURL+options.url:options.url}if(options.body&&options.headers&&!options.headers["Content-Type"]){options.headers["Content-Type"]="application/x-www-form-urlencoded"}options={...defaultOptions,...options};const timeout=options.timeout;const events={...{onRequest:()=>{},onResponse:(resp)=>resp,onTimeout:()=>{},},...options.events,};events.onRequest(method,options);let worker;if(isQX){worker=$task.fetch({method,...options})}else if(isLoon||isSurge||isNode||isShadowrocket||isStash){worker=new Promise((resolve,reject)=>{const request=isNode?require("request"):$httpClient;request[method.toLowerCase()](options,(err,response,body)=>{if(err)reject(err);else resolve({statusCode:response.status||response.statusCode,headers:response.headers,body,})})})}else if(isScriptable){const request=new Request(options.url);request.method=method;request.headers=options.headers;request.body=options.body;worker=new Promise((resolve,reject)=>{request.loadString().then((body)=>{resolve({statusCode:request.response.statusCode,headers:request.response.headers,body,})}).catch((err)=>reject(err))})}else if(isBrowser){worker=new Promise((resolve,reject)=>{fetch(options.url,{method,headers:options.headers,body:options.body,}).then((response)=>response.json()).then((response)=>resolve({statusCode:response.status,headers:response.headers,body:response.data,})).catch(reject)})}let timeoutid;const timer=timeout?new Promise((_,reject)=>{timeoutid=setTimeout(()=>{events.onTimeout();return reject(`${method}URL:${options.url}exceeds the timeout ${timeout}ms`)},timeout)}):null;return(timer?Promise.race([timer,worker]).then((res)=>{clearTimeout(timeoutid);return res}):worker).then((resp)=>events.onResponse(resp))}const http={};methods.forEach((method)=>(http[method.toLowerCase()]=(options)=>send(method,options)));return http}
